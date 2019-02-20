b31c02e4df9aa6a3dc2451d2f970b957c456b30f
Aditya Singh <adityasingh@Adityas-MacBook-Pro.local>
2019-02-20 14:13:00 +0530

    Pagination completed

--- blog/routes.py
+++ blog/routes.py
@@ -8,8 +8,10 @@ from flask_login import login_user,current_user,logout_user,login_required
 
 
 @app.route("/")
+@app.route("/home")
 def hello():
-	post=Post.query.all()
+	page=request.args.get('page',default=1,type=int)
+	post=Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
 	return render_template('home.html',posts=post)
 
 @app.route("/about")
@@ -139,7 +141,12 @@ def delete_post(post_id):
 	return redirect(url_for('hello'))
 
 
-
+@app.route("/user/<string:username>")
+def user_post(username):
+	page=request.args.get('page',default=1,type=int)
+	user=User.query.filter_by(username=username).first_or_404()
+	post=Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
+	return render_template('user_post.html',posts=post,user=user)
 
 
 
